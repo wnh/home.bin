cd630cfd3dee2f21261dacf05685ec30de0f0a67
Will Harding <wharding@avalanche.ca>
2018-11-30 08:42:46 -0800

    Add volume and day of week

--- cmd/dstatus/dstatus.go
+++ cmd/dstatus/dstatus.go
@@ -8,11 +8,14 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"os/exec"
+	"strings"
 	"time"
 
 	"github.com/BurntSushi/xgb"
 	"github.com/BurntSushi/xgb/xproto"
 )
+
 
 func main() {
 	x, err := xgb.NewConn()
@@ -20,11 +23,19 @@ func main() {
 		log.Fatal(err)
 	}
 	root := xproto.Setup(x).DefaultScreen(x).Root
-	for t := time.Tick(5 * time.Second); ; <-t {
-		status := fmt.Sprintf("%v  %v", battery(), timeStr())
+	for t := time.Tick(1 * time.Second); ; <-t {
+		status := doStatus()
 		xproto.ChangeProperty(x, xproto.PropModeReplace, root, xproto.AtomWmName,
 		xproto.AtomString, 8, uint32(len(status)), []byte(status))
 	}
+}
+
+func doStatus() string {
+	t := timeStr()
+	b :=  battery()
+	v :=  getVolume()
+
+	return fmt.Sprintf("v%v  b%v  %v", v, b, t)
 }
 
 func battery() string {
@@ -37,13 +48,17 @@ func battery() string {
 		symbol = "+"
 	} else if status == "Full" {
 		symbol = "!"
+	} else if status == "Unknown" {
+		symbol = "?"
 	}
 
-	return fmt.Sprintf("%v%v", symbol, cap)
+	return fmt.Sprintf("%v%v", cap, symbol)
 }
 
 func timeStr() string {
-	return time.Now().Format("2006/01/02 15:04")
+	d := time.Now()
+	dateStr := d.Format("Mon 2006/01/02 15:04")
+	return dateStr
 }
 
 func readString(fname string) string {
@@ -53,4 +68,25 @@ func readString(fname string) string {
 		os.Exit(1)
 	}
 	return string(bytes.TrimSpace(str))
+}
+
+func getVolume() string {
+	var outbuf bytes.Buffer
+	cmd := exec.Command("pacmd", "list-sinks")
+	cmd.Stdout = &outbuf
+	err := cmd.Run()
+	if err != nil {
+		fmt.Println("Error getting volume:", err)
+	}
+	out := outbuf.String()
+	outlines :=  strings.Split(out, "\n")
+	for _, l := range outlines {
+		clean := strings.TrimSpace(l)
+		if strings.HasPrefix(clean, "volume:") {
+			parts := strings.Split(clean, "/")
+			cleanVol := strings.TrimSpace(parts[1])
+			return cleanVol
+		}
+	}
+	return ""
 }
